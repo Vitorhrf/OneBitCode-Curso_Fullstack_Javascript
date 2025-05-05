dd2a3a2e267ad02ec5a4a0861bcfaafc55b721ce
Vítor Henrique <vitorhrf@outlook.com>
2025-05-05 19:14:58 -0300

    Segundo commit do exercicio 1

---
+++ GIT/Exercicio 1 - Commits/index.py
@@ -0,0 +1,11 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def main():
+    print("Bem-vindo ao conversor de temperaturas!")
+    temp_c = float(input("Digite a temperatura em Celsius: "))
+    temp_f = celsius_to_fahrenheit(temp_c)
+    print(f"{temp_c}°C é igual a {temp_f}°F")
+
+if __name__ == "__main__":
+    main()
