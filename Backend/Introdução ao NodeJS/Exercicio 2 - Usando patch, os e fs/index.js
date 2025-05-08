c7aed10b7df42fde3966e786bc951ba37d79a8cc
Vítor Henrique <vitorhrf@outlook.com>
2025-05-08 18:27:53 -0300

    Adição exercicio 2 de introdução ao nodejs

---
+++ Backend/Introdução ao NodeJS/Exercicio 2 - Usando patch, os e fs/index.js
@@ -0,0 +1,28 @@
+const os = require("node:os")
+const patch = require("node:path")
+const fs = require("node:fs")
+
+const dir = patch.resolve(__dirname, "./log")
+const logFile = patch.resolve(dir, "log.txt")
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir)
+}
+let logList = ""
+
+function watchSys() {
+    setInterval(() => { 
+        let log = `Sistema operacional: ${os.version()}
+Arquitetura do sistema: ${os.arch()}
+Modelo do processador: ${os.cpus()[0].model}
+Tempo de atividade do sistema: ${(os.uptime()/60 / 60).toFixed(1)} horas
+Uso de memória: ${((1 - os.freemem() / os.totalmem()) * 100).toFixed(1)}%
+`
+        logList += log + "\n"
+        console.clear()
+        console.log(log)
+        fs.writeFileSync(logFile, logList)
+    }, 1000)
+}
+
+watchSys()
+
