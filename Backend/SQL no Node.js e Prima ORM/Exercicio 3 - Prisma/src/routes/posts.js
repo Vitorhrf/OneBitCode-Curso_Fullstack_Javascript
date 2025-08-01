405232db459e447da4375267d826be5439ab4944
Vítor Henrique <vitorhrf@outlook.com>
2025-08-01 14:55:37 -0300

    Adição da rota search no posts por filtros

--- Backend/SQL no Node.js e Prima ORM/Exercicio 3 - Prisma/src/routes/posts.js
+++ Backend/SQL no Node.js e Prima ORM/Exercicio 3 - Prisma/src/routes/posts.js
@@ -8,6 +8,46 @@ router.get("/", async (req, res) => {
     res.json(posts)
 })
 
+router.get("/search", async (req, res) =>{
+    const { title, authorId, published, startDate, endDate } = req.query
+    const filter = {}
+
+    if (title) {
+        filter.title = {
+            contains: title,
+            mode: "insensitive"
+        }
+    }
+
+    if (authorId) {
+        filter.authorId = +authorId
+    }
+
+    if (published) {
+        filter.published = published === "true"
+    }
+
+    if (startDate || endDate) {
+        filter.createdAt = {}
+        if (startDate) {
+            filter.createdAt.gte = new Date(startDate)
+        }
+        
+        if (endDate) {
+            filter.createdAt.lte = new Date(endDate)
+        }
+    }
+
+    console.log("Filtros: ", filter)
+
+    const posts = await prisma.post.findMany({
+        where: filter,
+        orderBy: { createdAt: "desc" }
+    })
+
+    res.json(posts)
+})
+
 router.get("/:id", async (req, res) => {
     const post = await prisma.post.findUnique({
         where: { id: +req.params.id },
